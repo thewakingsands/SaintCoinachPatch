0aa4fc667da0b4e4f7a55dba03d34e2d0a0ba620
oott123 <git@public.oott123.com>
2019-01-28 15:06:29 +0800

    using fetch head, rev commit

--- bin/update.sh
+++ bin/update.sh
@@ -6,12 +6,13 @@ scdir=`pwd`/../SaintCoinach
 
 cd $scdir
 git fetch https://github.com/ufx/SaintCoinach.git
-git reset --hard origin/master
+git reset --hard FETCH_HEAD
+upstreamRev=`git rev-parse --short HEAD`
 cd $mydir
 bin/apply.sh
 cd $scdir
 mv $scdir/README.md $scdir/README_upstream.md
 cp $mydir/files/README_SaintCoinach.md $scdir/README.md
 git add .
-git commit -m "Patched for Chinese server use"
+git commit -m "Chinese server patch, based on $upstreamRev"
 git push -f origin cn
